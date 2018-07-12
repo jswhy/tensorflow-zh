3db7d2b88ea8b3cf0729f8eb663eeb1018274cb5
sean.jie <sean.jie>
2018-07-12 09:28:54 +0800

    create a yml file

---
+++ mycodes/c1s01.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import numpy as np
+
+# Create 100 phony x, y data points in NumPy, y = x * 0.1 + 0.3
+x_data = np.random.rand(100).astype("float32")
+y_data = x_data * 0.1 + 0.3
+
+# Try to find values for W and b that compute y_data = W * x_data + b
+# (We know that W should be 0.1 and b 0.3, but Tensorflow will
+# figure that out for us.)
+W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+b = tf.Variable(tf.zeros([1]))
+y = W * x_data + b
+
